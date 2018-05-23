e0edeb1a7436ed3a23b6eddf2c20193d70367f20
Florian Tim Kroschewski <florian@kroschewski.eu>
2018-05-23 16:30:59 +0200

    unit

---
+++ prozessliste.ps1
@@ -0,0 +1,24 @@
+﻿If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
+{   
+#"No Administrative rights, it will display a popup window asking user for Admin rights"
+
+$arguments = "& '" + $myinvocation.mycommand.definition + "'"
+Start-Process "$psHome\powershell.exe" -Verb runAs -ArgumentList $arguments
+
+break
+}
+#"After user clicked Yes on the popup, your file will be reopened with Admin rights"
+#"Put your code here"
+
+Set-ExecutionPolicy remotesigned
+Get-Process | Sort-Object CPU -Descending | Select-Object -First 10
+
+
+
+Write-Host -NoNewLine "Press any key to continue...";
+$null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
+
+
+
+
+Read-Host -Prompt "Press Enter to continue"
