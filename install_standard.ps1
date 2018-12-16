8fa600f7be3d6a3d67d61d5f3477f06f6fa524be
Unknown <florian@kroschewski.eu>
2018-12-16 22:36:20 +0100

    Add clementine. Use upgrade command that includes install

--- install_standard.ps1
+++ install_standard.ps1
@@ -8,28 +8,15 @@ If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdenti
     break
 }
 
-
-Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-Write-Host "script root: $PSScriptRoot";
-
 # standard
-choco install -y "$PSScriptRoot\standard\browser.config"
-choco install -y "$PSScriptRoot\standard\com.config"
-choco install -y "$PSScriptRoot\standard\foto.config"
-choco install -y "$PSScriptRoot\standard\mail.config"
-choco install -y "$PSScriptRoot\standard\media.config"
-choco install -y "$PSScriptRoot\standard\office.config"
-choco install -y "$PSScriptRoot\standard\tools.config"
-
-# Dev
-choco install -y "$PSScriptRoot\dev\atom.config"
-choco install -y "$PSScriptRoot\dev\c_and_cpp.config"
-choco install -y "$PSScriptRoot\dev\git.config"
-choco install -y "$PSScriptRoot\dev\python.config"
-choco install -y "$PSScriptRoot\dev\tools.config"
-choco install -y "$PSScriptRoot\dev\vm.config"
-choco install -y "$PSScriptRoot\dev\web.config"
-
+choco upgrade -y "$PSScriptRoot\standard\browser.config"
+choco upgrade -y "$PSScriptRoot\standard\com.config"
+choco upgrade -y "$PSScriptRoot\standard\foto.config"
+choco upgrade -y "$PSScriptRoot\standard\mail.config"
+choco upgrade -y "$PSScriptRoot\standard\media.config"
+choco upgrade -y "$PSScriptRoot\standard\office.config"
+choco upgrade -y "$PSScriptRoot\standard\tools.config"
+choco upgrade -y "$PSScriptRoot\standard\system.config"
 
 Write-Host -NoNewLine "Press any key to continue...";
 $null = $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown");
